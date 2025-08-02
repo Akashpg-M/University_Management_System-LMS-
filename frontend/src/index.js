0ca43437faf2331f5a1455962d618656a91a7fbb
akashpg-M <waybooster.pg@gmail.com>
2025-08-02 11:52:00 +0530

    University Management - frontend design alginments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './styles.css'; // Ensure you import your CSS styles if any
+import './styles.css'; // Ensure you import your CSS styles if any
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
